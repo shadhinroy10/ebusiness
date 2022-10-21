8fa5ffc4b475ff59a1c3f3aecd973338bba53337
shadhinroy10 <shadhins2001@gmail.com>
2022-10-22 00:23:23 +0600

    third

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -104,11 +104,114 @@
     </h2>
 </div>
 
-<div class="category-area">
-    <div class="card">
+<div class="category-area grid-eight">
+    <div class="card text-center">
+        <a href="#">
+            <div class="p-40">
         <div class="image">
             <img src="{{url('assets/frontend/images/laptop.png')}}" alt="">
         </div>
+        <div class="card-body text-center">
+            <p> All Laptop </p>
+        </div>
+    </div>
+    </a>
+    </div>
+
+    <div class="card text-center">
+        <a href="#">
+            <div class="p-40">
+        <div class="image">
+            <img src="{{url('assets/frontend/images/laptop.png')}}" alt="">
+        </div>
+        <div class="card-body text-center">
+            <p> All Laptop </p>
+        </div>
+    </div>
+    </a>
+    </div>
+
+
+    <div class="card text-center">
+        <a href="#">
+            <div class="p-40">
+        <div class="image">
+            <img src="{{url('assets/frontend/images/laptop.png')}}" alt="">
+        </div>
+        <div class="card-body text-center">
+            <p> All Laptop </p>
+        </div>
+    </div>
+    </a>
+    </div>
+
+
+    <div class="card text-center">
+        <a href="#">
+            <div class="p-40">
+        <div class="image">
+            <img src="{{url('assets/frontend/images/laptop.png')}}" alt="">
+        </div>
+        <div class="card-body text-center">
+            <p> All Laptop </p>
+        </div>
+    </div>
+    </a>
+    </div>
+
+
+    <div class="card text-center">
+        <a href="#">
+            <div class="p-40">
+        <div class="image">
+            <img src="{{url('assets/frontend/images/laptop.png')}}" alt="">
+        </div>
+        <div class="card-body text-center">
+            <p> All Laptop </p>
+        </div>
+    </div>
+    </a>
+    </div>
+
+
+    <div class="card text-center">
+        <a href="#">
+            <div class="p-40">
+        <div class="image">
+            <img src="{{url('assets/frontend/images/laptop.png')}}" alt="">
+        </div>
+        <div class="card-body text-center">
+            <p> All Laptop </p>
+        </div>
+    </div>
+    </a>
+    </div>
+
+
+    <div class="card text-center">
+        <a href="#">
+            <div class="p-40">
+        <div class="image">
+            <img src="{{url('assets/frontend/images/laptop.png')}}" alt="">
+        </div>
+        <div class="card-body text-center">
+            <p> All Laptop </p>
+        </div>
+    </div>
+    </a>
+    </div>
+
+    <div class="card text-center">
+        <a href="#">
+            <div class="p-40">
+        <div class="image">
+            <img src="{{url('assets/frontend/images/laptop.png')}}" alt="">
+        </div>
+        <div class="card-body text-center">
+            <p> All Laptop </p>
+        </div>
+    </div>
+    </a>
     </div>
 </div>
 
@@ -119,6 +222,78 @@
 
 
   {{--Category area end--}}
+
+  {{--product area end--}}
+<section class="product">
+     <div class="container product__container">
+
+        <div class="heading">
+            <h2 class="title">
+                Products
+            </h2>
+        </div>
+            <div class="product-area grid-five">
+                <div class="card">
+                    <div class="image">
+                        <img src="{{url('assets/frontend/images/kraken.jpg')}}" alt="">
+                    </div>
+ 
+                    <div class="card-body p-20">
+
+                        <p> <a href="#">HP Pavilion 15-eg1678TU Core i5 11th Gen 15.6" FHD Laptop</a> </p>
+                        <div class="row mt-15">
+                            <div class="cloumn-half">
+                                <p class="price ">Price: £1112</p>
+                            </div>
+
+                            <div class="cloumn-half">
+                                <p class="sku text-right"> <a href="#">SKU: Baaauu</a> </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="image">
+                        <img src="{{url('assets/frontend/images/kraken.jpg')}}" alt="">
+                    </div>
+ 
+                    <div class="card-body p-20">
+
+                        <p> <a href="#">HP Pavilion 15-eg1678TU Core i5 11th Gen 15.6" FHD Laptop</a> </p>
+                        <div class="row mt-15">
+                            <div class="cloumn-half">
+                                <p class="price ">Price: £1112</p>
+                            </div>
+
+                            <div class="cloumn-half">
+                                <p class="sku text-right"> <a href="#">SKU: Baaauu</a> </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+            
+     </div>
+
+</section>
+
+
+  {{--product area end--}}
+
+
+  {{--footer area start--}}
+
+<section class="footer">
+
+
+</section>
+
+  {{--footer area end--}}
 
 
     {{--jquery--}}
