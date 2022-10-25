10a0fbbb9788fe7768d6b94f4b060ad36933084b
shadhinroy10 <shadhins2001@gmail.com>
2022-10-26 01:57:45 +0600

    fifth

---
+++ resources/views/frontend/about/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.frontend')
+
+@section('content')
+
+{{-- about section start --}}
+
+<section>
+    <div class="container about__container"></div>
+    <h2>About Us</h2>
+</section>
+
+{{-- about section start --}}
+
+@endsection
